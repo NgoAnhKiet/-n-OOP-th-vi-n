7d01c94a24f901daa06474974ba9dd0dfd47e4dd
NgoAnhKiet <anhkietngo0805@gmail.com>
2024-02-18 15:24:55 +0700

    Chưa hoàn thành

---
+++ 16_02_2024_OnTapOOP/Main.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_02_2024_OnTapOOP
+{
+    class main
+    {
+        static void Main()
+        {
+            int n_Sach, n_TapChi, n_Bao;
+            Console.WriteLine("Moi nhap so luong mat hang!!!");
+            do
+            {
+                Console.Write("\nSach(0->10): ");
+                n_Sach = Convert.ToInt32(Console.ReadLine());
+            } while (n_Sach < 0 || n_Sach > 10);
+            Data_Of_Sach[] sach = new Data_Of_Sach[n_Sach];
+            Data_Of_Sach sachtemp = new Data_Of_Sach();  //KHONG DUNG METHOD STATIC THI PHAI KHAI 
+
+            do
+            {
+                Console.Write("\nTapchi(0->10): ");
+                n_TapChi = Convert.ToInt32(Console.ReadLine());
+            } while (n_TapChi < 0 || n_TapChi > 10);
+            Data_Of_TapChi[] tapchi = new Data_Of_TapChi[n_TapChi];
+            Data_Of_TapChi tapchitemp = new Data_Of_TapChi();  //KHONG DUNG METHOD STATIC THI PHAI KHAI 
+
+            do
+            {
+                Console.Write("\nBao(0->10): ");
+                n_Bao = Convert.ToInt32(Console.ReadLine());
+            } while (n_Bao < 0 || n_Bao > 10);
+            Data_Of_Bao[] bao = new Data_Of_Bao[n_Bao];
+            Data_Of_Bao baotemp = new Data_Of_Bao();  //KHONG DUNG METHOD STATIC THI PHAI KHAI 
+
+            if (n_Sach != 0)
+            {
+                for (int i = 0; i < n_Sach; i++)
+                {
+                    Console.WriteLine($"\nNhap thong tin sach thu {i+1}!!!");
+                    #region NHAP GIA TRI TRUC TIEP TRONG CLASS PROGRAM
+                    //Console.Write("Nhap ma tai lieu: ");
+                    //sach[i].MaTaiLieu = Console.ReadLine();
+                    //Console.Write("Nhap ten nha xuat ban: ");
+                    //sach[i].TenNhaXuatBan = Console.ReadLine();
+                    //Console.Write("Nhap so ban phat hanh: ");
+                    //sach[i].SoBanPhatHanh = Convert.ToInt32(Console.ReadLine());
+                    //Console.Write("Nhap ten tac gia: ");
+                    //sach[i].TenTacGia = Console.ReadLine();
+                    //Console.Write("Nhap so trang: ");
+                    //sach[i].SoTrang = Convert.ToInt32(Console.ReadLine());
+                    #endregion
+
+                    //KHONG DUNG METHOD STATIC, nhap gia tri qua method trong class Data_Of_Sach
+                    sachtemp.GetInfo_Sach(ref sach[i]);
+
+                    //DUNG METHOD STATIC thi KHOI can khai them sachtemp, dung luon <tenclass>.<ten method tatic>
+                    //Data_Of_Sach.GetInfo_Sach(ref sach[i]); 
+                }
+            }
+
+            if (n_TapChi != 0)
+            {
+                for (int i = 0; i < n_TapChi; i++)
+                {
+                    Console.WriteLine($"\nNhap thong tin tap chi thu {i + 1}!!!");
+                    #region NHAP GIA TRI TRUC TIEP TRONG CLASS PROGRAM
+                    //Console.Write("Nhap ma tai lieu: ");
+                    //tapchi[i].MaTaiLieu = Console.ReadLine();
+                    //Console.Write("Nhap ten nha xuat ban: ");
+                    //tapchi[i].TenNhaXuatBan = Console.ReadLine();
+                    //Console.Write("Nhap so ban phat hanh: ");
+                    //tapchi[i].SoBanPhatHanh = Convert.ToInt32(Console.ReadLine());
+                    //Console.Write("Nhap so phat hanh: ");
+                    //tapchi[i].SoPhatHanh = Convert.ToInt32(Console.ReadLine()) ;
+                    //Console.Write("Nhap thang phat hanh: ");
+                    //tapchi[i].ThangPhatHanh = Convert.ToInt32(Console.ReadLine());
+                    #endregion
+
+                    //KHONG DUNG METHOD STATIC, nhap gia tri qua method trong class Data_Of_TapChi
+                    tapchitemp.GetInfo_TapChi(ref tapchi[i]);
+
+                    //DUNG METHOD STATIC thi KHOI can khai them tapchitemp, dung luon <tenclass>.<ten method tatic>
+                    //Data_Of_TapChi.GetInfo_TapChi(ref tapchi[i]);
+
+                }
+            }
+
+            if (n_Bao != 0)
+            {
+                for (int i = 0; i < n_Bao; i++)
+                {
+                    Console.WriteLine($"\nNhap thong tin bao thu {i + 1}!!!");
+                    #region NHAP GIA TRI TRUC TIEP TRONG CLASS PROGRAM
+                    Console.Write("Nhap ma tai lieu: ");
+                    bao[i].MaTaiLieu = Console.ReadLine();
+                    Console.Write("Nhap ten nha xuat ban: ");
+                    bao[i].TenNhaXuatBan = Console.ReadLine();
+                    Console.Write("Nhap so ban phat hanh: ");
+                    bao[i].SoBanPhatHanh = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Nhap ngay phat hanh: ");
+                    bao[i].NgayPhatHanh = Convert.ToDateTime(Console.ReadLine());
+                    #endregion
+
+                    //KHONG DUNG METHOD STATIC, nhap gia tri qua method trong class Data_Of_Bao
+                    baotemp.GetInfo_Bao(ref bao[i]);
+
+                    //DUNG METHOD STATIC thi KHOI can khai them baotemp, dung luon <tenclass>.<ten method tatic>
+                    //Data_Of_Bao.GetInfo_Bao(ref bao[i]);
+
+                }
+            }
+
+
+            Console.WriteLine("\nCac du lieu vua nhap!!!");
+
+            if (n_Sach == 0) { Console.WriteLine("Khong co sach nao ca!!!"); }
+            else
+            {
+                //KHONG DUNG METHOD STATIC, xuat gia tri qua method trong class Data_Of_Sach
+                sachtemp.Xuat_Sach(sach, n_Sach);
+
+                //DUNG METHOD STATIC thi KHOI can khai them sachtemp, dung luon <tenclass>.<ten method tatic>
+                //Data_Of_Sach.Xuat_Sach(sach, n_Sach);
+            }
+
+            if (n_TapChi == 0) { Console.WriteLine("Khong co tap chi nao ca!!!"); }
+            else
+            {
+                //KHONG DUNG METHOD STATIC, xuat gia tri qua method trong class Data_Of_TapChi
+                tapchitemp.Xuat_TapChi(tapchi, n_TapChi);
+
+                //DUNG METHOD STATIC thi KHOI can khai them tapchitemp, dung luon <tenclass>.<ten method tatic>
+                //Data_Of_TapChi.Xuat_TapChi(tapchi, n_TapChi);
+            }
+
+            if (n_Bao == 0) { Console.WriteLine("Khong co Bao nao ca!!!"); }
+            else
+            {
+                //KHONG DUNG METHOD STATIC, xuat gia tri qua method trong class Data_Of_Bao
+                baotemp.Xuat_Bao(bao, n_Bao);
+
+                //DUNG METHOD STATIC thi KHOI can khai them baotemp, dung luon <tenclass>.<ten method tatic>
+                //Data_Of_Bao.Xuat_Bao(bao, n_Bao);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 16_02_2024_OnTapOOP/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_02_2024_OnTapOOP
+{
+    
+    class Data_Chung
+        {
+            protected string matailieu = "";
+            protected string tennhaxuatban = "";
+            protected int sobanphathanh = 0;
+        
+            
+            public string MaTaiLieu
+            {
+                get => matailieu;
+                set => matailieu = value;
+            }
+            public string TenNhaXuatBan
+            {
+                get => tennhaxuatban;
+                set => tennhaxuatban = value;
+            }
+            public int SoBanPhatHanh
+            {
+                get => sobanphathanh;
+                set => sobanphathanh = value;
+            }
+        }
+
+    class Data_Of_Sach : Data_Chung
+    {
+            private string tentacgia;
+            private int sotrang;
+            public string TenTacGia
+            {
+                get => tentacgia;
+                set => tentacgia = value;
+            }
+
+            public int SoTrang
+            {
+                get => sotrang;
+                set => sotrang = value;
+            }
+        //KHONG DUNG METHOD SATIC(Ham tinh)
+        public void GetInfo_Sach(ref Data_Of_Sach sach)
+        {
+            Console.Write("Nhap ma tai lieu: ");
+            sach.MaTaiLieu = Console.ReadLine();
+            Console.Write("Nhap ten nha xuat ban: ");
+            sach.TenNhaXuatBan = Console.ReadLine();
+            Console.Write("Nhap so ban phat hanh: ");
+            sach.SoBanPhatHanh = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap ten tac gia: ");
+            sach.TenTacGia = Console.ReadLine();
+            Console.Write("Nhap so trang: ");
+            sach.SoTrang = Convert.ToInt32(Console.ReadLine());
+        }
+        //DUNG METHOD STATIC(Ham tinh)
+        //public STATIC void GetInfo_Sach(ref Data_Of_Sach sach)
+
+        //KHONG DUNG METHOD SATIC(Ham tinh)
+        public void Xuat_Sach(Data_Of_Sach[] sach, int n_sach)
+        {
+            for (int i = 0; i < n_sach; i++)
+            {
+                Console.WriteLine($"\nSach thu {i + 1} ");
+                Console.WriteLine($"Ma tai lieu: {sach[i].MaTaiLieu}");
+                Console.WriteLine($"Ten nha xuat ban: {sach[i].TenNhaXuatBan}");
+                Console.WriteLine($"So ban phat hanh: {sach[i].SoBanPhatHanh}");
+                Console.WriteLine($"Ten tac gia: {sach[i].TenTacGia}");
+                Console.WriteLine($"So trang: {sach[i].SoTrang}");
+            }
+        }
+        //DUNG METHOD STATIC(Ham tinh)
+        //public STATIC void Xuat_Sach(Data_Of_Sach[] sach, int n_sach)
+
+    }
+
+    class Data_Of_TapChi : Data_Chung
+    {
+            private int sophathanh;
+            private int thangphathanh;
+            public int SoPhatHanh
+            {
+                get => sophathanh;
+                set => sophathanh = value;
+            }
+            public int ThangPhatHanh
+            {
+                get => thangphathanh;
+                set => thangphathanh = value;
+            }
+        //KHONG DUNG METHOD SATIC(Ham tinh)
+        public void GetInfo_TapChi(ref Data_Of_TapChi tapchi)
+        {
+            Console.Write("Nhap ma tai lieu: ");
+            tapchi.MaTaiLieu = Console.ReadLine();
+            Console.Write("Nhap ten nha xuat ban: ");
+            tapchi.TenNhaXuatBan = Console.ReadLine();
+            Console.Write("Nhap so ban phat hanh: ");
+            tapchi.SoBanPhatHanh = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap so phat hanh: ");
+            tapchi.SoPhatHanh = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap thang phat hanh: ");
+            tapchi.ThangPhatHanh = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+        //DUNG METHOD STATIC(Ham tinh)
+        //public STATIC void GetInfo_TapChi(ref Data_Of_TapChi tapchi)
+
+        //KHONG DUNG METHOD SATIC(Ham tinh)
+        public void Xuat_TapChi(Data_Of_TapChi[] tapchi, int n_tapchi)
+        {
+            for (int i = 0; i < n_tapchi; i++)
+            {
+                Console.WriteLine($"\nTap chi thu {i + 1} ");
+                Console.WriteLine($"Ma tai lieu: {tapchi[i].MaTaiLieu}");
+                Console.WriteLine($"Ten nha xuat ban: {tapchi[i].TenNhaXuatBan}");
+                Console.WriteLine($"So ban phat hanh: {tapchi[i].SoBanPhatHanh}");
+                Console.WriteLine($"So phat hanh: {tapchi[i].SoPhatHanh}");
+                Console.WriteLine($"Thang phat hanh: {tapchi[i].ThangPhatHanh}");
+            }
+        }
+        //DUNG METHOD STATIC(Ham tinh)
+        //public STATIC void Xuat_TapChi(Data_Of_TapChi[] tapchi, int n_tapchi)
+    }
+
+    class Data_Of_Bao : Data_Chung
+    {
+            private DateTime ngayphathanh;
+            public DateTime NgayPhatHanh
+            {
+                get => ngayphathanh;
+                set => ngayphathanh = value;
+            }
+        //KHONG DUNG METHOD SATIC(Ham tinh)
+        public void GetInfo_Bao(ref Data_Of_Bao bao)
+        {
+            Console.Write("Nhap ma tai lieu: ");
+            bao.MaTaiLieu = Console.ReadLine();
+            Console.Write("Nhap ten nha xuat ban: ");
+            bao.TenNhaXuatBan = Console.ReadLine();
+            Console.Write("Nhap so ban phat hanh: ");
+            bao.SoBanPhatHanh = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap ngay phat hanh: ");
+            bao.NgayPhatHanh = Convert.ToDateTime(Console.ReadLine());
+
+        }
+        //DUNG METHOD STATIC(Ham tinh)
+        //public STATIC void GetInfo_Bao(ref Data_Of_Bao bao)
+
+        //KHONG DUNG METHOD SATIC(Ham tinh)
+        public void Xuat_Bao(Data_Of_Bao[] bao, int n_bao)
+        {
+            for (int i = 0; i < n_bao; i++)
+            {
+                Console.WriteLine($"\nBao thu {i + 1} ");
+                Console.WriteLine($"Ma tai lieu: {bao[i].MaTaiLieu}");
+                Console.WriteLine($"Ten nha xuat ban: {bao[i].TenNhaXuatBan}");
+                Console.WriteLine($"So ban phat hanh: {bao[i].SoBanPhatHanh}");
+                Console.WriteLine($"Ngay phat hanh: {bao[i].NgayPhatHanh}");
+
+            }
+        }
+        //DUNG METHOD STATIC(Ham tinh)
+        //public STATIC void Xuat_Bao(Data_Of_Bao[] bao, int n_bao)
+    }
+
+    
+
+
+}
